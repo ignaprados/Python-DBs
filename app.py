e40f489d4e6a8fec908a8fb6d3f2e6d7fab9a97d
Ignacio Prados <ignacioprados.ip@gmail.com>
2023-07-21 01:31:58 -0300

    Add App

---
+++ app.py
@@ -0,0 +1,132 @@
+from neo4j import GraphDatabase
+import os
+import time
+
+class NeoApp:
+
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self.driver.close()
+
+    def crear_nodo_persona(self, nombre, dni, telefono, trabajo):
+        with self.driver.session() as session:
+            result = session.execute_write(self.crear_nodo_persona_neo, nombre, dni, telefono, trabajo)
+            print(result)
+
+    def crear_nodo_delito(self, tipo, descripcion, fecha, hora, barrio):
+        with self.driver.session() as session:
+            result = session.execute_write(self.crear_nodo_delito_neo, tipo, descripcion, fecha, hora, barrio)
+            print(result)
+
+    # Falta función para crear Relacion
+
+    def dueno_auto(self):
+        with self.driver.session() as session:
+            result = session.execute_write(self.dueno_auto_neo)
+            print(result)
+
+    def sospechosos_delito(self, delito):
+        with self.driver.session() as session:
+            result = session.execute_write(self.sospechosos_delito_neo, delito)
+            print(result)
+
+
+    @staticmethod
+    def crear_nodo_persona_neo(tx, nombre, dni, telefono, trabajo):
+        result = tx.run("CREATE (persona1:PERSONA{nombre:$nombre, documento:$documento, telefono:$telefono trabajo:$trabajo})", nombre=nombre, dni=dni, telefono=telefono, trabajo=trabajo)
+        return result.single()[0]
+
+    @staticmethod
+    def crear_nodo_delito_neo(tx, tipo, descripcion, fecha, hora, barrio):
+        result = tx.run("CREATE (delito1:DELITO{tipo:$robo, descripcion:$descripcion, fecha:$fecha, hora:$hora, barrio:$barrio})", tipo=tipo, descripcion=descripcion, fecha=fecha, hora=hora, barrio=barrio)
+        return result.single()[0]
+
+    @staticmethod
+    def dueno_auto_neo(tx):
+        result = tx.run("""MATCH (dueno:PERSONA)-[:DUENO]-> (bien:BIEN) WHERE ((:PERSONA{nombre: "Carlos Rojo"})-[:SOSPECHOSO]-> (:DELITO)-[:ASOCIADO]-> (bien:BIEN)) RETURN dueno.nombre""")
+        return result.single()[0]
+
+    @staticmethod
+    def sospechosos_delito_neo(tx, delito):
+        result = tx.run("""MATCH (sospechoso:PERSONA)-[:SOSPECHOSO]-> (delito:DELITO{descripcion:$delito}) RETURN sospechoso.nombre""", delito=delito)
+        return result.single()[0]
+
+
+
+
+
+print("Bienvenido")
+os.system("cls")
+print("Elija una operacion (ingrese el numero)")
+print("1. Crear un nodo")
+print("2. Crear una relacion")
+print("3. Quien es el duseño del auto por el cual se lo acusa a Carlos")
+print("4. Quienes son los sospechosos de un delito determinado")
+
+neo = NeoApp("bolt://localhost:7474", "neo4j", "uade1234")
+
+def menu():
+
+    opcion = input("numero de la operacionr: ")
+
+    if opcion == "1":
+        os.system("cls")
+        print("1 Crear un nodo ------------")
+
+        # input tipo de nodo (persona, delito)
+        tipo_nodo = input("tipo de nodo (persona, delito): ")
+
+        if tipo_nodo == "persona":
+            nombre = input("nombre: ")
+            dni = input("dni: ")
+            telefono = input("telefono: ")
+            trabajo = input("trabajo: ")
+
+            neo.crear_nodo_persona(nombre, dni, telefono, trabajo)
+
+        elif tipo_nodo == "delito":
+            tipo = input("tipo: ")
+            descripcion = input("descripcion: ")
+            fecha = input("fecha: ")
+            hora = input("fecha: ")
+            barrio = input("barrio: ")
+
+            neo.crear_nodo_delito(tipo, descripcion, fecha, hora, barrio)
+
+        else:
+            print("tipo de nodo incorrecto")
+
+        time.sleep(5)
+        menu()
+
+    elif opcion == "2":
+        os.system("cls")
+        print("2 Crear una relacion ------------")
+        time.sleep(5)
+        menu()
+
+
+    elif opcion == "3":
+        os.system("cls")
+        print("3 Quien es el duseño del auto por el cual se lo acusa a Carlos ------------")
+        neo.dueno_auto()
+        time.sleep(5)
+        menu()
+
+    elif opcion == "4":
+        os.system("cls")
+        print("4 Quienes son los sospechosos de un hecho determinado ------------")
+        delito = input("delito: ")
+        neo.sospechosos_delito(delito)
+        time.sleep(5)
+        menu()
+
+    else:
+        os.system("cls")
+        print("Opcion incorrecta")
+        menu()
+
+
+menu()
